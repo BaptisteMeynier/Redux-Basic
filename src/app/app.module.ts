846e7657da6a4aa1d99e6d27400e6cee36015f70
BaptisteMeynier <baptiste.meynier@gmail.com>
2019-09-17 22:02:04 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule } from '@ngrx/store'
 import { AppRoutingModule } from './app-routing.module';
+import { reducers } from './reducers/index'
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'
+import { EffectsModule } from '@ngrx/effects'
+import { CurrencyService } from './services/currency.service'
+import { CurrencyEffects } from './effects/currencyEffects'
 
 @NgModule({
   declarations: [
@@ -10,9 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([CurrencyEffects])
   ],
-  providers: [],
+  providers: [CurrencyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
